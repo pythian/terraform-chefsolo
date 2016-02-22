0085482f75efba41fc319ebd4844af14787d9d9c
Kevin Pedersen <byte_count@hotmail.com>
2016-02-21 19:50:51 -0500

    ditching web served chef-repo for now, using knife-solo instead

--- host_groups.tf
+++ host_groups.tf
@@ -10,20 +10,6 @@ resource "aws_security_group" "sg_theseeker_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
   egress {
     from_port = 0
     to_port = 0
