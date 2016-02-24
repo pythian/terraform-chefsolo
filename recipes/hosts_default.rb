b0e20cf60a80961ba947c461dd1e778d5d7913ae
Kevin Pedersen <byte_count@hotmail.com>
2016-02-24 15:00:17 -0500

    removing second entry altogether, template variable still rendered in comment

--- recipes/hosts_default.rb
+++ recipes/hosts_default.rb
@@ -3,9 +3,3 @@ hostsfile_entry '${chef-client-addr}' do
   hostname 'chef-client'
   action :append
 end
-
-#chef-workstation entry in /etc/hosts
-#hostsfile_entry '${chef-workstation-addr}' do
-#  hostname 'chef-workstation'
-#  action :append
-#end
