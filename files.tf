d88577d75a8b478946e8702dd71f9a362450b1e0
Kevin Pedersen <byte_count@hotmail.com>
2016-02-24 14:53:50 -0500

    commented chef-workstation addition, cycle error on plan

--- files.tf
+++ files.tf
@@ -2,6 +2,7 @@ resource "template_file" "recipe_hosts_default" {
   filename = "recipes/hosts_default.rb"
   vars {
     chef-client-addr = "${aws_instance.chef-client.private_ip}"
+#   chef-workstation-addr = "${aws_instance.chef-workstation.private_ip}" #cycle error
   }
 }
 
