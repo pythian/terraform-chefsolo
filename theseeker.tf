f25efec785a4b2d7e3dd964a775ee8c8a7b7079f
Kevin Pedersen <byte_count@hotmail.com>
2016-02-23 20:44:01 -0500

    added support for hostsfile recipe (to configure hostname chef-client)

--- theseeker.tf
+++ theseeker.tf
@@ -47,13 +47,22 @@ resource "aws_instance" "theseeker" {
       "sudo gem install knife-solo",
       "knife solo init chef-repo; cd chef-repo",
       "knife solo prepare ec2-user@localhost -i ~/.ssh/mykey; rm ../install.sh",
+
+      /* here we are rendering the chef-client address for configuration on theseeker */
+      "knife cookbook site download hostsfile",
+      "tar xvzf hostsfile*.tar.gz --directory cookbooks; rm hostsfile*.tar.gz; mkdir cookbooks/hostsfile/recipes",
+      "echo \"${template_file.recipe_hosts_default.rendered}\" >> cookbooks/hostsfile/recipes/defult.rb",
+
+      /* cookbooks needed to configure ntp, our default recipe */
       "knife cookbook site download ntp",
       "tar xvzf ntp*.tar.gz --directory cookbooks; rm ntp*.tar.gz",
       "knife cookbook site download windows", #needed for a stupid hardcoded dependency in the ntp cookbook
       "tar xvzf windows*.tar.gz --directory cookbooks; rm windows*.tar.gz",
       "knife cookbook site download chef_handler", #needed because there's no chef server
       "tar xvzf chef_handler*.tar.gz --directory cookbooks; rm chef_handler*.tar.gz",
-      "knife node --local-mode run_list add localhost 'recipe[ntp::default]'"
+
+      /* here we add the default cookbook and the default hostsfile recipe containing the chef client address */
+      "knife node --local-mode run_list add localhost 'recipe[ntp::default], recipe[hostsfile::default]'"
       ]
       connection {
         type = "ssh"
