4a229c141bf2c38608dc9c728a1043bd65ed71c7
Kevin Pedersen <byte_count@hotmail.com>
2016-02-23 19:17:04 -0500

    Added chef-client security group with access inbound from theseeker

--- host_groups.tf
+++ host_groups.tf
@@ -17,3 +17,22 @@ resource "aws_security_group" "sg_theseeker_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "sg_chef-client_access" {
+  name = "sg_theseeker_access"
+  description = "Allow inbound access to the chef-clients"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.sg_theseeker_access.id}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
