444bd375a4dda6e0745e2447a8e6497c30115dbc
Kevin Pedersen <byte_count@hotmail.com>
2016-02-24 14:54:39 -0500

    commentin chef-workstation entry, cycle error

--- recipes/hosts_default.rb
+++ recipes/hosts_default.rb
@@ -1,4 +1,11 @@
+# chef-client entry in /etc/hosts
 hostsfile_entry '${chef-client-addr}' do
   hostname 'chef-client'
+  action :append
+end
+
+#chef-workstation entry in /etc/hosts
+hostsfile_entry '${chef-workstation-addr}' do
+  hostname 'chef-workstation'
   action :append
 end
