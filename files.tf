892f7e023321be060538f1b7167ef68897ccee57
Kevin Pedersen <byte_count@hotmail.com>
2016-02-25 00:28:08 -0500

    update to rendered credentials template

--- files.tf
+++ files.tf
@@ -11,7 +11,6 @@ resource "template_file" "aws_credentials_file" {
   vars {
     aws_access_key = "${var.access_key}"
     aws_secret_key = "${var.secret_key}"
-    aws_region_id = "${var.region.primary}"
   }
 }
 
